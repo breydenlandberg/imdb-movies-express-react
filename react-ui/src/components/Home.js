7ed79554b0e122e8097a570ab4f05b31d2b0234d
breyden <breydenlandberg@outlook.com>
2021-09-25 14:17:02 +1000

    Alter Home appearance

--- react-ui/src/components/Home.js
+++ react-ui/src/components/Home.js
@@ -4,25 +4,25 @@
 const Home = () => {
   return (
     <div>
-      <section class='section is-medium has-background-white'>
+      <section class='section is-medium has-background-light'>
         <div class='columns is-centered is-full'>
-          <p class='title is-1 has-text-dark'> IMDb Movies Project by Breyden </p>
+          <p class='title is-1 has-text-black-bis'> IMDb Movies Project by Breyden </p>
         </div>
         <div class='columns is-centered is-full'>
-          <p class='subtitle is-3 has-text-dark'> built with PostgreSQL, Express, React, Node, and Bulma </p>
+          <p class='subtitle is-3 has-text-black-bis'> built with PostgreSQL, Express, React, Node, and Bulma </p>
         </div>
       </section>
 
       <section class='section is-large has-background-danger'>
         <div class='columns is-centered is-full'>
-          <p class='title is-1 has-text-black'> More text... </p>
+          <p class='title is-1 has-text-black-bis'> More text... </p>
         </div>
         <div class='columns is-centered is-full'>
-          <p class='subtitle is-3 has-text-black'> about something... </p>
+          <p class='subtitle is-3 has-text-black-bis'> about something... </p>
         </div>
       </section>
 
-      <section class='section is-large has-background-black'>
+      <section class='section is-large has-background-black-bis'>
         <div class='columns is-centered is-full'>
           <p class='title is-1 has-text-light'> Even more text... </p>
         </div>
