fe1e2dcefa8e4f971bd40855a4ca12b6aba697a7
breyden <breydenlandberg@outlook.com>
2021-09-25 14:16:48 +1000

    Alter IMDb movies page appearance

--- react-ui/src/components/movies/MovieLink.js
+++ react-ui/src/components/movies/MovieLink.js
@@ -7,36 +7,38 @@ const MovieLink = ({ data }) => {
   const { movie_id, movie_title, movie_release_year, movie_duration, movie_production_company, movie_description, movie_avg_vote, movie_votes, movie_user_reviews, movie_critic_reviews } = data
 
   return (
-    <div>
-      <ul>
-        <li key={movie_id}>
-          <Link to={{
-            pathname: `movies/${movie_id}`,
-            movieProps: {
-              movie_title,
-              movie_release_year,
-              movie_duration,
-              movie_production_company,
-              movie_description,
-              movie_avg_vote,
-              movie_votes,
-              movie_user_reviews,
-              movie_critic_reviews
-            }
-          }}
-          >
-            {movie_title}
-          </Link>
-          {movie_release_year}
-          {movie_duration}
-          {movie_production_company}
-          {movie_description}
-          {movie_avg_vote}
-          {movie_votes}
-          {movie_user_reviews}
-          {movie_critic_reviews}
-        </li>
-      </ul>
+    <div class='columns py-3'>
+      <div class='column is-three-fifths'>
+        <div class='box has-background-black-ter'>
+          <p class='title is-3 has-text-light'>
+            <Link to={{
+              pathname: `movies/${movie_id}`,
+              movieProps: {
+                movie_title,
+                movie_release_year,
+                movie_duration,
+                movie_production_company,
+                movie_description,
+                movie_avg_vote,
+                movie_votes,
+                movie_user_reviews,
+                movie_critic_reviews
+              }
+            }}
+            >
+              {movie_title}
+            </Link>
+          </p>
+          <p class='subtitle is-5 has-text-grey'> Subtitle </p>
+        </div>
+      </div>
+
+      <div class='column'>
+        <div class='box has-background-black-ter'>
+          <p class='title is-3 has-text-light'> movie_attributes </p>
+          <p class='subtitle is-5 has-text-grey'> Subtitle </p>
+        </div>
+      </div>
     </div>
   )
 }
--- react-ui/src/components/movies/Movies.js
+++ react-ui/src/components/movies/Movies.js
@@ -3,9 +3,20 @@ import MovieLink from './MovieLink'
 const MoviesPage = ({ data }) => {
   return (
     <div>
-      {data.map((movie) =>
-        <MovieLink data={movie} />
-      )}
+      <section class='section is-large has-background-black-bis'>
+        <div class='columns is-centered is-full'>
+          <p class='title is-1 has-text-light'> IMDb Movies Page </p>
+        </div>
+        <div class='columns is-centered is-full'>
+          <p class='subtitle is-3 has-text-light'> with subtitle text... </p>
+        </div>
+      </section>
+
+      <section class='section has-background-black-bis'>
+        {data.map((movie) =>
+          <MovieLink data={movie} />
+        )}
+      </section>
     </div>
   )
 }
