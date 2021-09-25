86cf0c8100117c21eacf8353bae9b9684051a1f7
breyden <breydenlandberg@outlook.com>
2021-09-26 00:27:39 +1000

    refactor

--- react-ui/src/components/LoadingScreen.js
+++ react-ui/src/components/LoadingScreen.js
@@ -1,6 +1,22 @@
+import loading from './loading.gif'
+
 const LoadingScreen = () => {
   return (
-    <div class='pageloader'><span class='title'>Pageloader</span></div>
+    <div>
+      <section class='section is-large has-background-black-bis'>
+        <div class='columns is-centered is-full'>
+          <p class='title is-1 has-text-light'> Fetching data for a smoother user experience... </p>
+        </div>
+
+        <div class='columns is-centered is-full'>
+          <p class='subtitle is-3 has-text-light'> *elevator music playing* </p>
+        </div>
+
+        <div class='columns is-centered is-full py-6'>
+          <img src={loading} />
+        </div>
+      </section>
+    </div>
   )
 }
 
